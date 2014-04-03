30c0eb7c9e4d8270e02fcc76f169d6eed47e8e69
woonohyo <wonhyo.yi@gmail.com>
2014-04-03 10:01:18 +0900

    Class 'Main' Implemented

---
+++ ACMICPC/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		//Fibonacci fibonacci = new Fibonacci();
+		//StringReverser strReverser = new StringReverser("This is a test string");
+		DistributedProcessing distProcess = new DistributedProcessing();
+	}
+}
--- ACMICPC/src/StringReverser.java
+++ ACMICPC/src/StringReverser.java
@@ -1,29 +1,31 @@
 import java.util.*;
 
 public class StringReverser {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String resultStr = reverseWords(sc.nextLine());
-		System.out.println(resultStr);
+	public StringReverser(String string) {
+		System.out.println(reverseUsingToken(string));
 	}
 
-	public static String reverseWords(String inputStr) {
+	public static String reverseUsingToken(String inputStr) {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(inputStr);
 		while (st.hasMoreTokens()) {
 			sb.insert(0, st.nextToken() + " ");
 		}
-		return sb.toString().trim();
 
-		// inputStr = inputStr.replaceAll(" +", " ");
-		//
-		// String reversedStr = new
-		// StringBuilder(inputStr).reverse().toString();
-		// for (String each : reversedStr.split(" ")) {
-		// sb.append(new StringBuilder(each).reverse().toString());
-		// sb.append(" ");
-		// }
-		// String resultStr = sb.toString().trim();
-		
+		return sb.toString().trim();
+	}
+
+	public static String reverseString(String inputStr) {
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(inputStr);
+		inputStr = inputStr.replaceAll(" +", " ");
+		String reversedStr = new StringBuilder(inputStr).reverse().toString();
+		for (String each : reversedStr.split(" ")) {
+			sb.append(new StringBuilder(each).reverse().toString());
+			sb.append(" ");
+		}
+		String resultStr = sb.toString().trim();
+		return resultStr;
+
 	}
 }

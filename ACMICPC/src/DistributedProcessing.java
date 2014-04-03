8adaf49788dd9ae840c2827ad4262798c6c8fba0
woonohyo <wonhyo.yi@gmail.com>
2014-04-03 11:51:02 +0900

    DistributedProcessing - minor fix

--- ACMICPC/src/DistributedProcessing.java
+++ ACMICPC/src/DistributedProcessing.java
@@ -6,17 +6,23 @@ public class DistributedProcessing {
 	public DistributedProcessing() {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Integer> seqOfLastDigit = new ArrayList<Integer>();
+		
 		int numOfTests = sc.nextInt();
 		int base, exponent;
 		BigInteger totalData;
+		
 		for (int idxOfTests = 0; idxOfTests < numOfTests; idxOfTests++) {
 			base = sc.nextInt();
 			exponent = sc.nextInt();
-			for (int i = 1; i < 5; i++) {
-				totalData = BigInteger.valueOf(base).pow(i);
-				seqOfLastDigit.add(totalData.mod(BigInteger.TEN).intValue() == 0 ? 10 : totalData.mod(BigInteger.TEN).intValue());
+
+			for (int exp = 0; exp < 4; exp++) {
+				totalData = BigInteger.valueOf(base).pow(exp + 1);
+				int modular = totalData.mod(BigInteger.TEN).intValue();
+				seqOfLastDigit.add(modular == 0 ? 10 : modular);
 			}
-			int fixedIdx = exponent % 4 == 0 ? 3 : exponent % 4 - 1;
+			
+			int fixedIdx = (exponent % 4 == 0) ? 3 : (exponent % 4 - 1);
+		
 			System.out.println(seqOfLastDigit.get(fixedIdx));
 			seqOfLastDigit.clear();
 		}

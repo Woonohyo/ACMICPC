de394b867fd2526e82628062a16a7c1fce548f22
woonohyo <wonhyo.yi@gmail.com>
2014-04-03 13:02:47 +0900

    Distributed Processing - minor fix

--- ACMICPC/src/DistributedProcessing.java
+++ ACMICPC/src/DistributedProcessing.java
@@ -1,31 +1,28 @@
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DistributedProcessing {
 	public DistributedProcessing() {
 		Scanner sc = new Scanner(System.in);
-		
-		// period of last digit of powered number is 4. (ex. 2 4 8 6, 2 4 8 6, ...)
+
+		// period of last digit of powered number is at most 4. (ex. 2 4 8 6, 2 4 8 6, ...)
 		ArrayList<Integer> seqOfLastDigit = new ArrayList<Integer>();
-		
 		int numOfTests = sc.nextInt();
-		int base, exponent;
-		int totalData;
-		
-		for (int idxOfTests = 0; idxOfTests < numOfTests; idxOfTests++) {
+		int base, exponent, totalData, modular, exp, idxOfTests;
+
+		for (idxOfTests = 0; idxOfTests < numOfTests; idxOfTests++) {
 			base = sc.nextInt();
 			exponent = sc.nextInt();
 
 			// Adding 4 last digits to the list
-			for (int exp = 0; exp < 4; exp++) {
+			for (exp = 0; exp < 4; exp++) {
 				totalData = (int) Math.pow(base, exp + 1);
-				int modular = totalData % 10;
+				modular = totalData % 10;
 				seqOfLastDigit.add(modular == 0 ? 10 : modular);
 			}
-			
+
 			int fixedIdx = (exponent % 4 == 0) ? 3 : (exponent % 4 - 1);
-		
+
 			System.out.println(seqOfLastDigit.get(fixedIdx));
 			seqOfLastDigit.clear();
 		}
